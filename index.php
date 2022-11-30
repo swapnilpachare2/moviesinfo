9001f227b63864efba322bcb0a5a44fb375c0336
Swapnil Pachare <118239384+swapnilpachare2@users.noreply.github.com>
2022-11-30 16:18:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-   header("Location: https://swapnpachare.000webhostapp.com/");
-   exit;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Resume</title>
+   <meta http-equiv="refresh" content="0; url = https://swapnpachare.000webhostapp.com/" />
+</head>
+<body>
+   
+</body>
+</html>
